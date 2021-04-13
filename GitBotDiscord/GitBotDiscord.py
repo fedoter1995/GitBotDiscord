c32664a283b3d3d7c149109436012ad0277301d2
fedoter1995 <fedoter1995@gmail.com>
2021-04-13 22:48:38 +0300

    Добавьте файлы проекта.

---
+++ GitBotDiscord/GitBotDiscord.py
@@ -0,0 +1,134 @@
+import asyncio
+
+import discord
+import youtube_dl
+
+from discord.ext import commands
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+ 
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        """Закинуть бота на канал"""
+
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
+
+    @commands.command()
+    async def play(self, ctx, *, query):
+        """Проигрывать с компа"""
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Ща играет: {query}')
+
+    @commands.command()
+    async def youtube(self, ctx, *, url):
+        """Проиграть по ссылке пока ютабчик только"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Ща играет: {player.title}')
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        """стрим пока тож только с ютабчика)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Ща играет: {player.title}')
+
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        """Громкость"""
+
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Gromkost na {volume}%")
+
+    @commands.command()
+    async def stop(self, ctx):
+        """Выгнать Бота!!"""
+
+        await ctx.voice_client.disconnect()
+
+    @play.before_invoke
+    @youtube.before_invoke
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("Вы не подключены к голосовому каналу.")
+                raise commands.CommandError("Главный не подключен к голосовому каналу.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+                   description='MusicBot')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} ({bot.user.id})')
+    print('------')
+
+bot.add_cog(Music(bot))
+bot.run('ODI5ODQ5Njk0MzYzOTc1NzIw.YG-HwA.hA8nFbDXoQZotBwnmRHHtQ_yErg')
